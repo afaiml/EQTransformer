39a6b2a22918cca380f55421faea3f4a07846ade
SSgt Bell <cbell@undclw81.st.aftac.gov>
2021-05-12 19:29:06 +0000

    Add tests for flask app

---
+++ tests/app_predict_test.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Friday May 7 18:45:00
+
+@author: cbell
+
+"""
+
+import requests
+import json
+
+predict_json = {"client": "IRIS",
+                "start_time": "2021-04-01 00:20:00.00",
+                "end_time": "2021-04-01 00:24:00.00"}
+
+head = {"Content-Type": "application/json"}
+
+
+predict_response = requests.post('http://127.0.0.1:5005/predict',
+                                 json=predict_json,
+                                 headers=head)
+
+print(predict_response)
+
+
