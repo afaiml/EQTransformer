d0587eb297262271a42652e8dc202e45bca5cced
SSgt Bell <cbell@undclw81.st.aftac.gov>
2021-06-01 15:04:44 +0000

    Fixed test case

--- tests/app_make_list_test.py
+++ tests/app_make_list_test.py
@@ -12,7 +12,7 @@ import requests
 
 make_list_json = {"client": "IRIS",
                   "stations": "SAO,MCCM",
-                  "start_time": "2050-04-01 00:20:00.00",
+                  "start_time": "2021-04-01 00:20:00.00",
                   "end_time": "2021-04-01 00:24:00.00"}
 
 head = {"Content-Type": "application/json"}
